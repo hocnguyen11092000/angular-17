b611ccee3d8b856300b6a77526db9f9250a1f1dc
ADMIN\Admin <hocnguyen1109200@gmail.com>
2024-03-25 22:44:34 +0700

    merge custom translate json

--- src/components/defer/defer.component.ts
+++ src/components/defer/defer.component.ts
@@ -8,12 +8,18 @@ import { LargeComponent } from '../large/large.component';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from '../cart/cart.component';
 import { TestService } from '../../services/test.service';
+import { I18nService } from '../../services/i18n.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { takeWhile, tap, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'app-defer',
   template: `
     <h3>hello from defer component</h3>
     <h3>{{ testService.name }}</h3>
+    <h1>{{ 'HOME.TITLE' | translate }}</h1>
+    <h1>{{ 'TEST' | translate }}</h1>
     <button #btn style="margin-bottom: 20px;">show component</button>
     @defer(on interaction(btn)) {
     <app-large>
@@ -25,11 +31,26 @@ import { TestService } from '../../services/test.service';
   `,
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [LargeComponent, CommonModule, CartComponent],
+  imports: [LargeComponent, CommonModule, CartComponent, TranslateModule],
   // providers: [TestService],
 })
 export class DeferComponent implements OnInit {
   readonly testService = inject(TestService);
+  private readonly i18Service = inject(I18nService);
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('assets/i18n/custom.json')
+      .pipe(
+        tap((res) => {
+          this.i18Service.setTranslation('vi', res, true);
+        }),
+        withLatestFrom(this.i18Service.getTranslation('vi'))
+      )
+      .subscribe((res) => {
+        console.log(res, this.i18Service.store);
+      });
+  }
 
   ngOnInit(): void {
     // this.testService.setName('defer');
